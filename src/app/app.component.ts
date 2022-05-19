59d51b9c8068dd8985fcda8bbc530d83d938cc73
Ferenc <guby002@gmail.com>
2022-05-19 10:25:08 +0200

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component } from '@angular/core';
+
+import { AccountService } from './_services';
+import {User} from "./_models";
+
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+  get user(): User {
+    return this._user;
+  }
+
+  set user(value: User) {
+    this._user = value;
+  }
+    private _user: User;
+
+    constructor(private accountService: AccountService) {
+        this.accountService.user.subscribe(x => this._user = x);
+    }
+
+    logout() {
+        this.accountService.logout();
+    }
+}
---
+++ src/app/users/list.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { AccountService } from '../_services';
+
+@Component({ templateUrl: 'list.component.html' })
+export class ListComponent implements OnInit {
+    users = null;
+
+    constructor(private accountService: AccountService) {}
+
+    ngOnInit() {
+        this.accountService.getAll()
+            .pipe(first())
+            .subscribe(users => this.users = users);
+    }
+
+    deleteUser(username: string) {
+        const user = this.users.find(x => x.id === username);
+        user.isDeleting = true;
+        this.accountService.delete(username)
+            .pipe(first())
+            .subscribe(() => this.users = this.users.filter(x => x.id !== username));
+    }
+}
